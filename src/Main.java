866b0e6d0620b7ccd10493f39ad0ec8efda8e123
Sacr1fice <sanunkurov@gmail.com>
2023-06-13 11:52:55 +0300

    dorabotka

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket = 13_676;
-        int onemile = 20;
+        int oneMile = 20;
 
-        int bonus = ticket / onemile;
+        int bonus = ticket / oneMile;
 
         System.out.println("Количество начисленных миль: " + bonus);
     }
